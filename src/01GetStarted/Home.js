9c29a48979c7399551fea476dfc8ae4a8e845a2f
MFMercado <ar.mflmercado@gmail.com>
2022-07-12 17:50:47 +0800

    updated home and about us

--- src/01GetStarted/Home.js
+++ src/01GetStarted/Home.js
@@ -6,21 +6,29 @@ import imgHeader from "../Images/img-building-websites.svg";
 
 const Home = () => {
   return (
-    <Container>
-      <Row>
-        <Col className={styles.div_1}>
-          <Image className={styles.imgHeader} src={imgHeader} alt="" />
-        </Col>
-        <Col className={styles.div_1}>
-          <h1 className={styles.heading_1}>Full Stack Developers</h1>
-          <p className={styles.para_1}>
-            We love tinkering the ins and outs of the web. <br /> Frontend,
-            backend, we got you covered.
-          </p>
-        </Col>
-      </Row>
-      <Button className={styles.button_1}>Contact Us</Button>
-    </Container>
+    <div
+      className={
+        styles.Home + " d-flex align-self-center justify-content-center"
+      }
+    >
+      <Container>
+        <Row>
+          <Col className={styles.div_1}>
+            <Image className={styles.imgHeader} src={imgHeader} alt="" />
+          </Col>
+          <Col className={styles.div_1}>
+            <h1 className={styles.heading_1 + " fontMain fontColorMain"}>
+              Full Stack Developers
+            </h1>
+            <p className={styles.para_1}>
+              We love tinkering the ins and outs of the web. <br /> Frontend,
+              backend, we got you covered.
+            </p>
+          </Col>
+        </Row>
+        <Button className={styles.button_1}>GET STARTED</Button>
+      </Container>
+    </div>
   );
 };
 
--- src/02AboutUs/AboutUs.js
+++ src/02AboutUs/AboutUs.js
@@ -1,38 +1,32 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Image, Button } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 import styles from "./AboutUs.module.css";
 import imgAbout from "../Images/img-team-spirit.svg";
 
 const AboutUs = () => {
-	return (
-		<Container>
-			<Row>
-				<Col>
-					<div className={styles.div_about}>
-						<Image className={styles.imgAbout} src={imgAbout} alt="" />
-					</div>
-				</Col>
-			</Row>
-			<Row>
-				<Col>
-					<div className={styles.div_about_title}>
-						<h1>ABOUT US</h1>
-						<p>
-							We came from different walks of life. One of us is an architect,
-							the other is an accountant, and the last one is an engineer. We
-							like to call ourselves, a self-taught developers. Our diverse
-							background gave us an advantage in dealing with problems. Each
-							problem in your business is unique. Coming from wide experiences,
-							one of us will be able to relate how that problem affects your
-							business, and in return we can offer you solutions that are out of
-							the box.{" "}
-						</p>
-					</div>
-				</Col>
-			</Row>
-		</Container>
-	);
+  return (
+    <div className={styles.AboutUs}>
+      <Container>
+        <div className={styles.div_about}>
+          <Image className={styles.imgAbout} src={imgAbout} alt="" />
+        </div>
+
+        <div className="text-center mt-5">
+          <h1 className="my-3 fontMain fontColorMain">ABOUT US</h1>
+          <p className="mb-5">
+            We came from different walks of life. One of us is an architect, the
+            other is an accountant, and the last one is an engineer. We like to
+            call ourselves, a self-taught developers. Our diverse background
+            gave us an advantage in dealing with problems. Each problem in your
+            business is unique. Coming from wide experiences, one of us will be
+            able to relate how that problem affects your business, and in return
+            we can offer you solutions that are out of the box.
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default AboutUs;
