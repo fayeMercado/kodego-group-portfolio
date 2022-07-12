ebb1b5ab7e77f03aa52f36bbe659eda8b5eb9df5
MFMercado <ar.mflmercado@gmail.com>
2022-07-12 14:33:16 +0800

    added team profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import "./App.css";
 import AppContactUs from "./AppContactUs/AppContactUs";
 import AppServices from "./AppServices/AppServices";
+import AppTeamProfile from "./AppTeamProfile/AppTeamProfile";
+import { MemberProfile } from "./AppTeamProfile/AppTeamProfile";
 
 function App() {
   return (
     <div className="App w-75 container-fluid">
       <h1>Start here</h1>
+      <AppTeamProfile>
+        <MemberProfile
+          firstName="MARWIN"
+          lastName="ALMENDRAS"
+          description="This is where our short description will be. Like, Lorem ipsum dolor sit
+      amet, consectetur adipiscing elit."
+          likes="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        />
+        <MemberProfile
+          firstName="MARIA FE"
+          lastName="MERCADO"
+          description="This is where our short description will be. Like, Lorem ipsum dolor sit
+      amet, consectetur adipiscing elit."
+          likes="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        />
+        <MemberProfile
+          firstName="KRISTOFF"
+          lastName="VILLANUEVA"
+          description="This is where our short description will be. Like, Lorem ipsum dolor sit
+      amet, consectetur adipiscing elit."
+          likes="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        />
+      </AppTeamProfile>
       <AppServices />
       <AppContactUs />
     </div>
