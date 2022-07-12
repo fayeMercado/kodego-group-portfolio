0fd1acfe3ea26d17bf0a82c58f4068db5ba240a8
Kristoff Villanueva <kristoffer.villanueva18@gmail.com>
2022-07-12 10:40:52 +0800

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Start here</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>CareerShifters</h1>
+			<p>This is a team portfolio</p>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
